93230665e4d01970971e3ef5955db656445aac9d
unknown <matheussardagna02@gmail.com>
2024-06-01 23:31:51 -0300

    tentando

--- Trabalho_geradorSenhas/src/main/java/org/example/Main.java
+++ Trabalho_geradorSenhas/src/main/java/org/example/Main.java
@@ -4,18 +4,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Usuario usuario;
-        System.out.println("Escreva o nome do usuario: ");
-        String nome = scanner.nextLine();
-
-        System.out.println("Escreva o email do usuario: ");
-        String email = scanner.nextLine();
-
-        usuario = new Usuario(nome,email);
+        Usuario usuario = new Usuario();
+        String nome = "sadal";
+        String email = "matheussardagna02@gmail.com";
 
         usuario.definirCriterios(12, true, true, true, true);
         String senha = usuario.gerarSenha();
         System.out.println("Senha gerada: " + senha);
+        usuario.setNome(nome);
+        usuario.setEmail(email);
+        usuario.setSenha(senha);
+        usuario.adicionarUsuario();
     }
 }
